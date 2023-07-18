8962af76c637c9deda8dbd231cf89433c23a96a8
Alancittoo <echeniquealan@yahoo.com>
2023-07-17 22:51:52 -0400

    two sum python added

---
+++ codewars.py
@@ -0,0 +1,30 @@
+# Two Sum problem
+
+# Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different items in the array that, when added together, give the target value. The indices of these items should then be returned in a tuple / list (depending on your language) like so: (index1, index2).
+
+# For the purposes of this kata, some tests may have multiple answers; any valid solutions will be accepted.
+
+# The input will always be valid (numbers will be an array of length 2 or greater, and all of the items will be numbers; target will always be the sum of two different items from that array).
+
+# Based on: http://oj.leetcode.com/problems/two-sum/
+numbers = [ 11, 2,  7, 15]
+target = 9
+
+def two_sum(numbers, target):
+    #     keep track of numbers we go through
+    nums_left = {}
+
+    for i in range(len(numbers)):
+        num = numbers[i]
+#         number in array - target number
+        left = target - num
+#         if left is within the numbs left, return it
+        if left in nums_left:
+            return [nums_left[left], i]
+
+        nums_left[num] = i
+
+test = two_sum(numbers, target)
+
+
+print(test) # [ 1 , 2 ]
