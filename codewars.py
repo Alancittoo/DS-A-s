a64eccd89c68cd8fb15abbb1c0960b3f6a7f0aee
Alancittoo <echeniquealan@yahoo.com>
2023-07-25 22:05:51 -0400

    generate hashtag problem solved

--- codewars.py
+++ codewars.py
@@ -142,3 +142,34 @@ def is_valid_walk(walk):
         coords[x] += 1
 
     return coords['n'] == coords['s'] and coords['e'] == coords['w']
+
+
+# The marketing team is spending way too much time typing in hashtags.
+# Let's help them with our own Hashtag Generator!
+
+# Here's the deal:
+
+# It must start with a hashtag (#).
+# All words must have their first letter capitalized.
+# If the final result is longer than 140 chars it must return false.
+# If the input or the result is an empty string it must return false.
+# Examples
+# " Hello there thanks for trying my Kata"  =>  "#HelloThereThanksForTryingMyKata"
+# "    Hello     World   "                  =>  "#HelloWorld"
+# ""                                        =>  false
+
+def generate_hashtag(s):
+    if not s:
+        return False
+
+    words = s.split()
+    hashtag = '#'
+
+    for word in words:
+        capitalized_word = word.capitalize()
+        hashtag += capitalized_word
+
+    if len(hashtag) > 140:
+        return False
+
+    return hashtag
