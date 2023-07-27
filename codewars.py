aafccc00d318c9cf311581c81175e79998b4698e
Alancittoo <echeniquealan@yahoo.com>
2023-07-27 10:30:28 -0400

    like system problem

--- codewars.py
+++ codewars.py
@@ -188,3 +188,28 @@ def count_bits(n):
 
     ones = bin_num.count('1') #count 1s
     return ones
+
+
+# You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+
+# Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+
+# []                                -->  "no one likes this"
+# ["Peter"]                         -->  "Peter likes this"
+# ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+# ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+# ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+
+def likes(names):
+    res = len(names)
+
+    if res == 0:
+        return "no one likes this"
+    elif res == 1:
+        return f"{names[0]} likes this"
+    elif res == 2:
+        return f"{names[0]} and {names[1]} like this"
+    elif res == 3:
+        return f"{names[0]}, {names[1]} and {names[2]} like this"
+    else:
+        return f"{names[0]}, {names[1]} and {res - 2} others like this"
