9c4c4c16e838694937276b5b2594ae54a1615a87
Alancittoo <echeniquealan@yahoo.com>
2023-07-30 22:42:36 -0400

    completed non repeating letter problem

--- codewars.py
+++ codewars.py
@@ -285,4 +285,25 @@ def times(num):
     return lambda x: x * num
 
 def divided_by(num):
-    return lambda x: x // num 
+    return lambda x: x // num
+
+#Write a function named first_non_repeating_letter that takes a string input, and returns the first character that is not repeated anywhere in the string.
+
+#For example, if given the input 'stress', the function should return 't', since the letter t only occurs once in the string, and occurs first in the string.
+
+#As an added challenge, upper- and lowercase letters are considered the same character, but the function should return the correct case for the initial letter. For example, the input 'sTreSS' should return 'T'.
+
+#If a string contains all repeating characters, it should return an empty string ("") or None -- see sample tests.
+
+def first_non_repeating_letter(s):
+    lower = s.lower()
+    res = {}
+
+    for letter in lower:
+        res[letter] = res.get(letter, 0) + 1
+
+    for letter in s:
+        if res[lower[s.index(letter)]] == 1:
+            return letter
+
+    return ""
