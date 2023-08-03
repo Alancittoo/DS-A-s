4ef62e8a8d43f072b0f54cdaa7b1d788df45c3aa
Alancittoo <echeniquealan@yahoo.com>
2023-08-02 23:47:34 -0400

    find next square fundamental completed

--- debug&fundamentals.py
+++ debug&fundamentals.py
@@ -22,3 +22,28 @@ def get_count(sentence):
         if let in vowels:
             start += 1
     return start
+
+
+
+
+# You might know some pretty large perfect squares. But what about the NEXT one?
+
+# Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+
+# If the parameter is itself not a perfect square then -1 should be returned. You may assume the parameter is non-negative.
+
+# Examples:(Input --> Output)
+
+# 121 --> 144
+# 625 --> 676
+# 114 --> -1 since 114 is not a perfect square
+
+
+import math
+def find_next_square(sq):
+    if math.sqrt(sq).is_integer():
+        num = math.sqrt(sq)
+        num += 1
+        return num * num
+    else:
+        return -1
