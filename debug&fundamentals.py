b25c94ca6354d6c9586e2a0ec8dc7aae280fcc83
Alancittoo <echeniquealan@yahoo.com>
2023-08-03 18:13:35 -0400

    square sum

--- debug&fundamentals.py
+++ debug&fundamentals.py
@@ -47,3 +47,16 @@ def find_next_square(sq):
         return num * num
     else:
         return -1
+
+
+# Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+# For example, for [1, 2, 2] it should return 9
+
+
+def square_sum(numbers):
+    sum = 0
+
+    for num in numbers:
+        sum += num * num
+    return sum
