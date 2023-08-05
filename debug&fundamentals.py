709252fdfbde94e2ca5d25f77d184eba8b2bbf9f
Alancittoo <echeniquealan@yahoo.com>
2023-08-04 23:13:15 -0400

    quick DS&A due to time

--- debug&fundamentals.py
+++ debug&fundamentals.py
@@ -60,3 +60,13 @@ def square_sum(numbers):
     for num in numbers:
         sum += num * num
     return sum
+
+
+# Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+
+def bool_to_word(boolean):
+    if boolean == True:
+        return "Yes"
+    else:
+        return "No"
