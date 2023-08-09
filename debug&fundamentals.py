77e7b293d996675ba12251b3f29ece457200ba0d
Alancittoo <echeniquealan@yahoo.com>
2023-08-09 15:45:39 -0400

    DNA strand complete

--- debug&fundamentals.py
+++ debug&fundamentals.py
@@ -70,3 +70,24 @@ def bool_to_word(boolean):
         return "Yes"
     else:
         return "No"
+
+
+
+# Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+
+# If you want to know more: http://en.wikipedia.org/wiki/DNA
+
+# In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+# More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+
+# Example: (input --> output)
+
+# "ATTGC" --> "TAACG"
+# "GTAT" --> "CATA"
+
+
+def DNA_strand(dna):
+    dna_pal = {'A': 'T', 'T': 'A', 'G' : 'C', 'C' : 'G'}
+    res = [dna_pal[x] for x in dna]
+    return ''.join(res)
