cd69a442baa3fa5bd2cc5dbc0247911187f12abe
Alancittoo <echeniquealan@yahoo.com>
2023-08-31 23:05:27 -0400

    string interpolation excercise

--- codewars.py
+++ codewars.py
@@ -346,3 +346,10 @@ def solution(string):
 
 def string_to_number(s):
     return int(s)
+
+# Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+# [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+def greet(name):
+    return f"Hello, {name} how are you doing today?"
