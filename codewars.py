ed1e30eb35887f3ea1a8f98bf477b52ff6a11724
Alancittoo <echeniquealan@yahoo.com>
2023-10-04 17:31:48 -0400

    smallest num in array

--- codewars.py
+++ codewars.py
@@ -415,3 +415,22 @@ def permutations(s):
     solo = set()
     test('', s)
     return list(solo)
+
+
+# Given an array of integers your solution should find the smallest integer.
+
+# For example:
+
+# Given [34, 15, 88, 2] your solution will return 2
+# Given [34, -345, -1, 100] your solution will return -345
+# You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
+
+def find_smallest_int(arr):
+    #needs to be first
+    small = arr[0]
+    #anything after first
+    for i in arr[1:]:
+        if i < small:
+            small = i
+    return small
