d3d3ce9c1dbd00b49f2be0420ed49911c6a8febf
Alancittoo <echeniquealan@yahoo.com>
2023-11-11 12:59:16 -0500

    codewars clock problem

--- codewars.py
+++ codewars.py
@@ -465,3 +465,28 @@ def get_grade(s1, s2, s3):
         return 'D'
     else:
         return 'F'
+
+
+# Clock shows h hours, m minutes and s seconds after midnight.
+
+# Your task is to write a function which returns the time since midnight in milliseconds.
+
+# Example:
+# h = 0
+# m = 1
+# s = 1
+
+# result = 61000
+# Input constraints:
+
+# 0 <= h <= 23
+# 0 <= m <= 59
+# 0 <= s <= 59
+
+def past(h, m, s):
+    res = 0
+    nh = 3600000
+    nm = 60000
+    ns = 1000
+    res += nh + ns + nm
+    return res
