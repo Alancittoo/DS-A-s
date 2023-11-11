2e9c97287978d61e619b85fbd5ecfae71a1a1e05
Alancittoo <echeniquealan@yahoo.com>
2023-11-11 18:23:40 -0500

    decimal debug

--- debug&fundamentals.py
+++ debug&fundamentals.py
@@ -227,3 +227,16 @@ class Warrior():
             else:
                 self.zombie = True
     # he would be a zombie only if he was already dead
+
+
+# Since there are lots of katas requiring you to round numbers to 2 decimal places, you decided to extract the method to ease out the process.
+
+# And you can't even get this right!
+
+# Quick, fix the bug before everyone in CodeWars notices that you can't even round a number correctly!
+
+
+from decimal import Decimal, ROUND_HALF_UP
+
+def round_by_2_decimal_places(n):
+    return n.quantize(Decimal('0.01'),ROUND_HALF_UP)
