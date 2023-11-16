43b9aa2838d3bf99d1b8020aeb14cb7828f12ab1
Alancittoo <echeniquealan@yahoo.com>
2023-11-15 22:27:57 -0500

    num to string codewars

--- codewars.py
+++ codewars.py
@@ -569,3 +569,15 @@ def scramble(s1,s2):
         else:
             dct[l] -= 1
         return True
+
+# We need a function that can transform a number (integer) into a string.
+
+# What ways of achieving this do you know?
+
+# Examples (input --> output):
+# 123  --> "123"
+# 999  --> "999"
+# -100 --> "-100"
+
+def number_to_string(num):
+    return str(num)
